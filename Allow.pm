5be75f9a83acd5ac43490cc4ddf99076d3552744
Nigel Horne <njh@bandsman.co.uk>
2014-09-10 10:40:48 -0400

    First public release

---
+++ Allow.pm
@@ -0,0 +1,105 @@
+package CGI::Allow;
+
+# Author Nigel Horne: njh@bandsman.co.uk
+# Copyright (C) 2014, Nigel Horne
+
+# Usage is subject to licence terms.
+# The licence terms of this software are as follows:
+# Personal single user, single computer use: GPL2
+# All other users (including Commercial, Charity, Educational, Government)
+#	must apply in writing for a licence for use from Nigel Horne at the
+#	above e-mail.
+
+# Decide if we're going to allow this client to view the website
+# Usage:
+# unless(CGI::Allow::allow({info => $info, lingua => $lingua})) {
+
+use Carp;
+
+our %blacklist = (
+	'BY' => 1,
+	'MD' => 1,
+	'RU' => 1,
+	'CN' => 1,
+	'BR' => 1,
+	'UY' => 1,
+	'TR' => 1,
+	'MA' => 1,
+	'VE' => 1,
+	'SA' => 1,
+	'CY' => 1,
+	'CO' => 1,
+	'MX' => 1,
+	'IN' => 1,
+	'RS' => 1,
+	'PK' => 1,
+	'UA' => 1,
+);
+
+our $status = -1;
+
+sub allow {
+	my %args = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
+
+	my $info = $args{'info'};
+
+	unless($status == -1) {
+		# Cache the value
+		return $status;
+	}
+
+	if(!defined($info)) {
+		carp('Info not given');
+		$status = 1;
+		return 1;
+	}
+
+	unless($info->is_search_engine() || !defined($ENV{'REMOTE_ADDR'})) {
+		require Data::Throttler;
+		Data::Throttler->import();
+
+		# Handle YAML Errors
+		my $db_file = $info->tmpdir() . '/throttle';
+		eval {
+			my $throttler = Data::Throttler->new(
+				max_items => 15,
+				interval => 90,
+				backend => 'YAML',
+				backend_options => {
+					db_file => $db_file
+				}
+			);
+
+			unless($throttler->try_push(key => $ENV{'REMOTE_ADDR'})) {
+				$status = 0;
+				return 0;
+			}
+		};
+		if($@) {
+			unlink($db_file);
+		}
+
+		my $lingua = $args{'lingua'};
+		if(defined($lingua) && $blacklist{uc($lingua->country())}) {
+			$status = 0;
+			return 0;
+		}
+
+		my $params = $info->params();
+		if(defined($params)) {
+			require CGI::IDS;
+			CGI::IDS->import();
+
+			my $ids = CGI::IDS->new();
+			$ids->set_scan_keys(scan_keys => 1);
+			if($ids->detect_attacks(request => $params) > 0) {
+				$status = 0;
+				return 0;
+			}
+		}
+	}
+	$status = 1;
+	return 1;
+}
+
+1;
--- README.md
+++ README.md
@@ -2,3 +2,22 @@ CGI-Allow
 =========
 
 Decide whether to allow a client to run a CGI script
+
+Install the package in some path.  The prerequesits are:
+
+CGI::Info
+CGI::Lingua
+Data::Throttler
+Carp
+CGI::IDS
+
+The run the code:
+
+my $info = CGI::Info->new();
+my $lingua = CGI::Lingua->new();
+
+unless(CGI::Allow::allow({ info => $info, lingua => $lingua }) {
+	print "403: Forbidden\n\n";
+	print "Go away!\n";
+	die "Disallow access";
+}
