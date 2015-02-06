e7e91f6f3ff8f7bedfc284e6ab7d42f3bf0dc87a
Nigel Horne <njh@bandsman.co.uk>
2015-02-06 16:06:19 -0500

    Check IP address against DShield's database

--- Allow.pm
+++ Allow.pm
@@ -1,7 +1,7 @@
 package CGI::Allow;
 
 # Author Nigel Horne: njh@bandsman.co.uk
-# Copyright (C) 2014, Nigel Horne
+# Copyright (C) 2014-2015, Nigel Horne
 
 # Usage is subject to licence terms.
 # The licence terms of this software are as follows:
@@ -43,11 +43,17 @@ sub allow {
 		# Cache the value
 		return $status;
 	}
+	if(!defined($ENV{'REMOTE_ADDR'})) {
+		# Not running as a CGI
+		$status = 1;
+		return 1;
+	}
 
 	my %args = (ref($_[0]) eq 'HASH') ? %{$_[0]} : @_;
 
 	my $info = $args{'info'};
 	my $logger = $args{'logger'};
+
 	if($logger) {
 		$logger->trace('In ' . __PACKAGE__);
 	}
@@ -62,7 +68,7 @@ sub allow {
 		return 1;
 	}
 
-	unless($info->is_search_engine() || !defined($ENV{'REMOTE_ADDR'})) {
+	unless($info->is_search_engine()) {
 		require Data::Throttler;
 		Data::Throttler->import();
 
@@ -126,16 +132,72 @@ sub allow {
 			$v = Data::Validate::URI->new();
 			unless($v->is_uri($ENV{'HTTP_REFERER'})) {
 				if($logger) {
-					$logger->info("Blocked shelshocker for $ENV{HTTP_REFERER}");
+					$logger->info("Blocked shellshocker for $ENV{HTTP_REFERER}");
 				}
 				$status = 0;
 				return 0;
 			}
 		}
 	}
-	if($logger) {
-		$logger->debug('Allowing connexion');
+
+	require DateTime;
+	DateTime->import();
+
+	my $cache = $args{'cache'};
+	my @ips;
+	my $today = DateTime->today()->ymd();
+	my $readfromcache;
+
+	if(defined($cache)) {
+		my $cachecontent = $cache->get($today);
+		if($cachecontent) {
+			if($logger) {
+				$logger->debug("read from cache $cachecontent");
+			}
+			@ips = split(/,/, $cachecontent);
+			$readfromcache = 1;
+		}
 	}
+
+	unless($ips[0]) {
+		require LWP::Simple;
+		LWP::Simple->import();
+		require XML::LibXML;
+		XML::LibXML->import();
+
+		if($logger) {
+			$logger->trace('Downloading DShield signatures');
+		}
+		my $xml = XML::LibXML->load_xml(string => get('https://secure.dshield.org/api/sources/attacks/100/2012-03-08'));
+		foreach my $source ($xml->findnodes('/sources/data')) {
+			my $lastseen = $source->findnodes('./lastseen')->to_literal();
+			next unless($lastseen eq $today);  # FIXME: Should be today or yesterday to avoid midnight rush
+			push @ips, $source->findnodes('./ip')->to_literal();
+		}
+		if(defined($cache) && !$readfromcache) {
+			my $cachecontent = join(',', @ips);
+			if($logger) {
+				$logger->debug("setting cache to $cachecontent");
+			}
+			$cache->set($today, $cachecontent, '1 day');
+		}
+	}
+
+	foreach my $ip (@ips) {
+		# FIXME: Doesn't realise 1.2.3.4 is the same as 001.002.003.004
+		if($ip eq $ENV{'REMOTE_ADDR'}) {
+			if($logger) {
+				$logger->info("Dshield blocked connexion from $ENV{REMOTE_ADDR}");
+			}
+			$status = 0;
+			return 0;
+		}
+	}
+
+	if($logger) {
+		$logger->trace("Allowing connexion from $ENV{REMOTE_ADDR}");
+	}
+
 	$status = 1;
 	return 1;
 }
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 CGI-Allow
 =========
 
-Decide whether to allow a client to run a CGI script
+Decide whether to allow a client to run a CGI script.  Clearly this should not be your only line of defence, but
+then neither should iptables, mod_security, etc.
 
 Install the package in some path.  The prerequesits are:
 
@@ -10,17 +11,25 @@ CGI::Lingua
 Data::Throttler
 Carp
 CGI::IDS
+DateTime;
+LWP::Simple;
+XML::LibXML;
+
+You can pass a logger argument for tracing what's going on.  logger should point to an object which takes methods such as trace,
+debug, warn, info.  Log4perl is a good example.
+
+Daily DShield signatures are downloaded.  To speed things up a *lot* you should use give the cache argument, otherwise each
+call will result in a data download from dshield.org and you really don't want that. The cache object is an object which
+takes get and set as methods.  I use CHI.
 
 The run the code:
 
 my $info = CGI::Info->new();
 my $lingua = CGI::Lingua->new();
+my $cache = $CHI->new(driver => 'BerkeleyDB', root_dir => $info->tmpdir(), namespace => $info->script_name());
 
-unless(CGI::Allow::allow({ info => $info, lingua => $lingua }) {
+unless(CGI::Allow::allow({ info => $info, lingua => $lingua, cache => $cache }) {
 	print "403: Forbidden\n\n";
 	print "Go away!\n";
 	die "Disallow access";
 }
-
-You can pass a logger argument for tracing what's going on.  logger should point to an object which takes methods such as trace,
-debug, warn, info.  Log4perl is a good example.
